68bf38d6c9214941ef3dd7187878822e5b93a36c
Jatin Pathak <jat.pat.rt22@dypatil.edu>
2024-07-26 21:12:48 +0530

    add results.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
+
 import InputValue from "./components/InputValue";
+import Result from "./components/Results"
 
 function App() {
+
+  const [userInput , setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 1,
+  });
+  
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleInput(inputIdentifier , newValue){
+    setUserInput(prevUserInput => {
+        return{
+            ...prevUserInput,
+            [inputIdentifier]: +newValue,
+        };
+    });
+  }
+
   return (
     <>
-      <InputValue />
+      <InputValue onChangeInput={handleInput} userInput={userInput}/>
+      {!inputIsValid && <p className="center">Please enter a duration greater than 0</p>}
+      { inputIsValid && <Result input={userInput}/>}
     </>
   );
 }
